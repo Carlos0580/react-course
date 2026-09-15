7c10337094748e859cb14a42f528b920aa3f4f11
Carlos J Sanchez G <carlosjesus0580@gmail.com>
2026-09-14 23:21:56 -0400

    1ra clase de reforzamiento

---
+++ src/bases/01-const-let.ts
@@ -0,0 +1,11 @@
+
+
+const firstName: string = 'Carlos';
+let   lastName : string = 'Sanchez';
+
+let diceNumber: number = 5
+
+
+const constainsLetterC: boolean = firstName.includes('C');
+
+console.log( {constainsLetterC, diceNumber, firstName, lastName })
