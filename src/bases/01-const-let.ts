5cec951118787ec13172ffa733dbfb4ec0cd5881
Carlos J Sanchez G <carlosjesus0580@gmail.com>
2026-09-15 22:51:15 -0400

    template string

--- src/bases/01-const-let.ts
+++ src/bases/01-const-let.ts
@@ -8,4 +8,4 @@ let diceNumber: number = 5
 
 const constainsLetterC: boolean = firstName.includes('C');
 
-console.log( {constainsLetterC, diceNumber, firstName, lastName })
+console.log( {constainsLetterC, diceNumber, firstName, lastName });
---
+++ src/bases/02-template-string.ts
@@ -0,0 +1,18 @@
+
+
+const firstName: string = 'Carlos';
+const   lastName : string = 'O\'Neal "es el apellido de alguien!"';
+
+
+const fullName = `${ firstName} ${ lastName }`
+
+
+
+
+
+
+
+
+
+console.log( lastName );
+console.log( fullName );
