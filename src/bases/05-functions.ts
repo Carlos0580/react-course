43ee6b49bc8a2508c0cfedef331ece6206fce698
Carlos J Sanchez G <carlosjesus0580@gmail.com>
2026-09-16 21:33:02 -0400

    functions

---
+++ src/bases/05-functions.ts
@@ -0,0 +1,34 @@
+
+
+function greet( name: string ): string {
+    return `Hola ${ name }`;
+}
+
+const greet2 = ( name: string ): string => {
+     return `Hola ${ name }`;
+}
+
+const msg = greet( 'Goku' );
+const msg2 = greet( 'Vegeta' );
+console.log({ msg, msg2 });
+
+
+function getUser(){
+   return{
+     uId: 'ABC-123',
+    username: 'El_papi23'   
+
+   }
+}
+
+const getUser2 = () => {
+    return{
+        uId: 'ABC-123',
+        username: 'El_papi23'  
+    }
+}
+
+const user = getUser();
+const user2 = getUser2();
+
+console.log( user, user2 );
