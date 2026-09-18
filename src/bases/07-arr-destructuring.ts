39ffb550b9a66eacd8d1e526056775ccbe28eb0c
Carlos J Sanchez G <carlosjesus0580@gmail.com>
2026-09-17 20:51:56 -0400

    tarea de desctructuring-realizada

--- src/bases/07-arr-destructuring.ts
+++ src/bases/07-arr-destructuring.ts
@@ -14,8 +14,33 @@ const returnArrayFn = () => {
 
 };
 
-
 const [ letters, number ] = returnArrayFn()
+console.log( number, letters);
 
-console.log( number + 100);
 
+
+
+
+// const [name, setName] = useState('Gogeta');
+// console.log(name);        Gogeta
+// setName('Vejito');       Imprime "Vejito"
+
+// const useState = () => {
+//      return [ 'Gogeta',  () =>  ( 'Vejito' ) ]  as const
+    
+// }
+// const [ letters2, setName2 ] = useState()
+//  console.log( letters2, setName2);
+
+const useState = (value: string) => {
+    return[
+        value,
+        ( newValue: string) =>{
+            console.log( newValue );
+        }
+    ] as const;
+};
+
+const [name, setName] = useState('Goku');
+console.log(name);       // Goku
+setName('Vegeta');       // Imprime "Vegeta"
